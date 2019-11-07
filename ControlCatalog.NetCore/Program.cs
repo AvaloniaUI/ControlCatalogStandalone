d39fbec811fc768b69b1f864b04053f661a5b2ea
Hank Grabowski <hankgrabowski@gmail.com>
2019-11-07 09:00:46 -0500

    Updated to version in main repo from master linked against 0.9.0-preview4

--- ControlCatalog.NetCore/Program.cs
+++ ControlCatalog.NetCore/Program.cs
@@ -1,25 +1,52 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Threading;
 using Avalonia;
-using Avalonia.Skia;
 using Avalonia.ReactiveUI;
+using Avalonia.Dialogs;
 
 namespace ControlCatalog.NetCore
 {
     static class Program
     {
-
-        static void Main(string[] args)
+        [STAThread]
+        static int Main(string[] args)
         {
-            Thread.CurrentThread.TrySetApartmentState(ApartmentState.STA);
-            BuildAvaloniaApp().Start(AppMain, args);
-        }
+            if (args.Contains("--wait-for-attach"))
+            {
+                Console.WriteLine("Attach debugger and use 'Set next statement'");
+                while (true)
+                {
+                    Thread.Sleep(100);
+                    if (Debugger.IsAttached)
+                        break;
+                }
+            }
 
-        static void AppMain(Application app, string[] args)
-        {
-            app.Run(new MainWindow());
+            var builder = BuildAvaloniaApp();
+
+            double GetScaling()
+            {
+                var idx = Array.IndexOf(args, "--scaling");
+                if (idx != 0 && args.Length > idx + 1 &&
+                    double.TryParse(args[idx + 1], NumberStyles.Any, CultureInfo.InvariantCulture, out var scaling))
+                    return scaling;
+                return 1;
+            }
+            if (args.Contains("--fbdev"))
+            {
+                SilenceConsole();
+                return builder.StartLinuxFbDev(args, scaling: GetScaling());
+            }
+            else if (args.Contains("--drm"))
+            {
+                SilenceConsole();
+                return builder.StartLinuxDrm(args, scaling: GetScaling());
+            }
+            else
+                return builder.StartWithClassicDesktopLifetime(args);
         }
 
         /// <summary>
@@ -28,7 +55,29 @@ namespace ControlCatalog.NetCore
         public static AppBuilder BuildAvaloniaApp()
             => AppBuilder.Configure<App>()
                 .UsePlatformDetect()
+                .With(new X11PlatformOptions
+                {
+                    EnableMultiTouch = true,
+                    UseDBusMenu = true
+                })
+                .With(new Win32PlatformOptions
+                {
+                    EnableMultitouch = true,
+                    AllowEglInitialization = true
+                })
                 .UseSkia()
-                .UseReactiveUI();
+                .UseReactiveUI()
+                .UseManagedSystemDialogs();
+
+        static void SilenceConsole()
+        {
+            new Thread(() =>
+            {
+                Console.CursorVisible = false;
+                while (true)
+                    Console.ReadKey(true);
+            })
+            { IsBackground = true }.Start();
+        }
     }
 }
--- ControlCatalog/App.xaml.cs
+++ ControlCatalog/App.xaml.cs
@@ -1,13 +1,37 @@
+using System;
 using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 
 namespace ControlCatalog
 {
     public class App : Application
     {
+        private NativeMenu _recentMenu;
+
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
+
+            Name = "Avalonia";
+
+            _recentMenu = (NativeMenu.GetMenu(this).Items[1] as NativeMenuItem).Menu;
+        }
+
+        public void OnOpenClicked(object sender, EventArgs args)
+        {
+            _recentMenu.Items.Insert(0, new NativeMenuItem("Item " + (_recentMenu.Items.Count + 1)));
+        }
+
+        public override void OnFrameworkInitializationCompleted()
+        {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktopLifetime)
+                desktopLifetime.MainWindow = new MainWindow();
+            else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewLifetime)
+                singleViewLifetime.MainView = new MainView();
+            
+            base.OnFrameworkInitializationCompleted();
         }
     }
 }
